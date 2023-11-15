32ea06e884dc1f9fc5fc16053e696108e2f69d17
Henry Kinoshita <42702870+PEROLOKO@users.noreply.github.com>
2023-11-15 03:24:46 -0300

    Initial commit

---
+++ src/main/java/com/dermahelp/DermahelpApplication.java
@@ -0,0 +1,13 @@
+package com.dermahelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DermahelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DermahelpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dermahelp/DermahelpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dermahelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DermahelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
